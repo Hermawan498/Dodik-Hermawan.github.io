d70b21291bcc05b34543186dc5d6a5c947e543ce
Hermawan498 <dodikhermawan09@gmail.com>
2024-06-20 21:54:45 +0700

    update perbaikan color, dan penambahan contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,16 @@ module.exports = {
     extend: {
       colors: {
         dark: '#020617',
-        biru:'#0ea5e9'
+        biru:'#0ea5e9' ,
+        case: '#1d4ed8',
+        font: '#1e3a8a',
+        abu: '#111827'
       },
     screen:{
+      'sm': '480px',
       '2xl' : '1320px',
       'xxs': {"max": "359px"},
-      'xs': {"max": "639px"},
-      "lg-md": "1024px",
+      "lg-md": "1024px"
     }  
     },
   },
